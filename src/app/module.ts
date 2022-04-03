834175a3f6802cda3c4b9c56714cd09692109efe
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-04-03 16:23:17 +0300

    Added logger

--- src/app/module.ts
+++ src/app/module.ts
@@ -2,8 +2,29 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AccountsModule } from "accounts/module";
 import { AccountProvidersModule } from "accountProviders/module";
+import { WinstonModule } from "nest-winston";
+import * as winston from "winston";
 
 @Module({
-	imports: [ConfigModule.forRoot(), AccountsModule, AccountProvidersModule],
+	imports: [
+		ConfigModule.forRoot(),
+		WinstonModule.forRoot({
+			level: "info",
+			format: winston.format.combine(
+				winston.format.timestamp(),
+				winston.format.json()
+			),
+			exitOnError: false,
+			transports: [
+				new winston.transports.File({
+					filename: "logs/error.log",
+					level: "error",
+				}),
+				new winston.transports.File({ filename: "logs/combined.log" }),
+			],
+		}),
+		AccountsModule,
+		AccountProvidersModule,
+	],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "app/module";
 import { MicroserviceOptions } from "@nestjs/microservices";
 import config from "config";
+import { WINSTON_MODULE_NEST_PROVIDER } from "nest-winston";
 
 async function bootstrap() {
 	const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -12,6 +13,7 @@ async function bootstrap() {
 			},
 		}
 	);
+	app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
 	await app.listen();
 }
